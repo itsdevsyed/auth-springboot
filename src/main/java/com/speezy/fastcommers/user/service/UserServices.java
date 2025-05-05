1d96643e341279bbc8933af52b1e857700115407
itsdevsyed <shoaib.syed4@outlook.com>
2025-05-05 18:32:38 +0530

    added the servcies which need to for each user either its user , delivery boy or the shoponwer

---
+++ src/main/java/com/speezy/fastcommers/user/service/UserServices.java
@@ -0,0 +1,36 @@
+package com.speezy.fastcommers.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.speezy.fastcommers.user.dto.UserDto;
+import com.speezy.fastcommers.user.entity.User;
+import com.speezy.fastcommers.user.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+
+
+@Service
+@Transactional
+public class UserServices {
+
+    @Autowired
+    private UserRepository userRepository;
+    public User registerUser(UserDto userDto) {
+
+        if(userRepository.existsByphoneNumber(userDto.getPhoneNumber())) {
+            throw new IllegalArgumentException("User already exists with this phone number");
+        }
+        User user =  User.builder()
+                 .name(userDto.getName())
+                 .phoneNumber(userDto.getPhoneNumber())
+                 .email((userDto.getEmail()))
+                 .role(userDto.getRole())
+                 .build();
+
+                 return userRepository.save(user);
+
+    }
+
+}
